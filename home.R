4644bb5bf04b46c6e9c8a8fc0f9136d639a6d01d
Alfie <aclong@users.noreply.github.com>
2019-11-22 12:42:14 +0000

    added geocode function

--- home.R
+++ home.R
@@ -5,3 +5,13 @@ home_town <- c("london")
 scared_of <= c("ghosts")
 
 df <- data.frame(name, home_town, scared_of)
+
+#geocoding data
+
+df <- df[, c("name","home_town","scared_of")]
+
+dfGeo <- tmaptools::geocode_OSM(as.vector(df$home_town), as.data.frame = TRUE)
+df <- merge(df, dfGeo, by.x="home_town", by.y="query")
+
+print(df)
+
