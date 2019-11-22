fd550e612a335fb15eeaf0b28824a5a8af7b0254
Alfie <aclong@users.noreply.github.com>
2019-11-22 12:44:52 +0000

    get rid of scared of

--- home.R
+++ home.R
@@ -2,13 +2,12 @@
 
 name <- c("alfie")
 home_town <- c("london")
-scared_of <= c("ghosts")
 
-df <- data.frame(name, home_town, scared_of)
+df <- data.frame(name, home_town)
 
 #geocoding data
 
-df <- df[, c("name","home_town","scared_of")]
+df <- df[, c("name","home_town")]
 
 dfGeo <- tmaptools::geocode_OSM(as.vector(df$home_town), as.data.frame = TRUE)
 df <- merge(df, dfGeo, by.x="home_town", by.y="query")
